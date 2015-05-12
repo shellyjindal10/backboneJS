4414b992474539b01ed2645aa0661e901f3b0f2f
shelly jindal <shellyjindal@shellys-MacBook-Pro.local>
2015-05-12 16:28:05 -0700

    for the app.js checkins

--- blogRollApp/js/app.js
+++ blogRollApp/js/app.js
@@ -1,4 +1,5 @@
 
+//Backbone Model
 var Blog = Backbone.Model.extend({
 	defaults:{
 		author:'',
@@ -6,3 +7,27 @@ var Blog = Backbone.Model.extend({
 		url:''
 	}
 })
+
+//Backbone Collections 
+var Blogs = Backbone.Collection.extend({
+
+})
+
+//instantiate the 2 blogs 
+var blogOne = new Blog({
+	author:"shelly",
+	title:"Explore Me Fully",
+	url:"www.google.com"
+})
+
+var blogTwo = new Blog({
+	author:"sudip",
+	title:"Extraordinary things ",
+	url:"www.yahoo.com"
+})
+
+//instantiate the collections 
+var blogs = new Blogs([blogOne,blogTwo]);
+
+//Blog Views 
+
